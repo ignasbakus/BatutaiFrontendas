97bbbfbb1a2ecc767eec6f69320431145e0fcfc4
LeBrounius <ignas.bakus77@gmail.com>
2024-01-25 16:35:21 +0200

    Pirmas ikelimas

---
+++ src/Header.js
@@ -0,0 +1,35 @@
+import React from "react";
+import "./Header.css";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+
+function Header(props) {
+  return (
+    <div>
+      <Navbar fixed="top" expand="md" className="bg-body-tertiary">
+        <Navbar.Brand className="ms-4" href="#home">
+          {props.brand}
+        </Navbar.Brand>
+        <Nav.Link className="me-4 ms-4" href="#home">
+          Home
+        </Nav.Link>
+        <Nav.Link className="me-4" href="#link">
+          Link
+        </Nav.Link>
+        <NavDropdown className="me-4" title="Dropdown" id="basic-nav-dropdown">
+          <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+          <NavDropdown.Divider />
+          <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+        </NavDropdown>
+          <Nav.Link className="ms-auto me-4" href="#link">
+            Prisijungti
+          </Nav.Link>
+      </Navbar>
+    </div>
+  );
+}
+
+export default Header;
