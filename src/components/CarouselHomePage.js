b08987828b3e1b76aa4b29dad74e4c9759bf5127
LeBrounius <ignas.bakus77@gmail.com>
2024-01-28 16:45:52 +0200

    Su ekrano dydžio kitimu keičiasi navbaras

--- src/components/CarouselHomePage.js
+++ src/components/CarouselHomePage.js
@@ -1,26 +1,32 @@
 import Carousel from "react-bootstrap/Carousel";
-import "../Styles/CarouselHomePage.css"
+import "../Styles/CarouselHomePage.css";
 
 function CarouselHomePage() {
   return (
     <>
       <Carousel data-bs-theme="dark">
-        <Carousel.Item className="carouselItem" interval={500}>
-          <img className="w-50" src={require("../images/Skraiduolis1.jpg")}/>
+        <Carousel.Item className="carouselItem" /*interval={5000}*/>
+          <img className="w-50" src={require("../images/Skraiduolis1.jpg")} />
           <Carousel.Caption>
             <h3>First slide label</h3>
             <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
           </Carousel.Caption>
         </Carousel.Item>
-        <Carousel.Item className="carouselItem" interval={500}>
-          <img className="w-50" src={require("../images/IF-1925_600x450px_1-800x800_0.jpg")} />
+        <Carousel.Item className="carouselItem" /*interval={5000}*/>
+          <img
+            className="w-50"
+            src={require("../images/IF-1925_600x450px_1-800x800_0.jpg")}
+          />
           <Carousel.Caption>
             <h3>Second slide label</h3>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
           </Carousel.Caption>
         </Carousel.Item>
-        <Carousel.Item className="carouselItem" interval={500}>
-          <img className="w-50" src={require("../images/pripuciamas-batutas-pilis-su-baseinu.jpg")} />
+        <Carousel.Item className="carouselItem" /*interval={5000}*/>
+          <img
+            className="w-50"
+            src={require("../images/pripuciamas-batutas-pilis-su-baseinu.jpg")}
+          />
           <Carousel.Caption>
             <h3>Third slide label</h3>
             <p>
--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,8 +1,9 @@
 import React from "react";
 import "../Styles/NavigationBar.css";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
+import Container from 'react-bootstrap/Container'; 
+import Nav from 'react-bootstrap/Nav'; 
+import Navbar from 'react-bootstrap/Navbar'; 
+import NavDropdown from 'react-bootstrap/NavDropdown'; 
 import { Link } from "react-router-dom";
 import { useState, useRef, useEffect } from "react";
 
@@ -27,42 +28,51 @@ function NavigationBar(props) {
   }, []);
 
   return (
-    <div>
-      <Navbar fixed="top" expand="md" className="bg-body-tertiary">
-        <Navbar.Brand className="ms-4 brandName" href="/">
-          {props.brand}
-        </Navbar.Brand>
-        <NavDropdown
-          className="me-4 ms-4"
-          title={props.items}
-          id="basic-nav-dropdown"
-          show={show}
-          onMouseEnter={showDropdown}
-          onMouseLeave={hideDropdown}
-          ref={dropdownRef}
-        >
-          <NavDropdown.Item href="#action/3.1">
-            {props.dropdownValue1}
-          </NavDropdown.Item>
-          <NavDropdown.Item href="#action/3.2">
-            {props.dropdownValue2}
-          </NavDropdown.Item>
-          <NavDropdown.Item href="">
-            {props.dropdownValue3}
-          </NavDropdown.Item>
-        </NavDropdown>
-        <Link className="me-4 navBarLinks" to="/reservation">
-          {props.reservation}
-        </Link>
-        <Link className="me-4 navBarLinks" to="/">{props.home}</Link>
-        <Link className="me-4 navBarLinks" to="/contact">
-          {props.contacts}
-        </Link>
-        <Link className="ms-auto me-4 navBarLinks" to="/login">
-          {props.login}
-        </Link>
+    <>
+      <Navbar collapseOnSelect expand="md" className="bg-body-tertiary">
+          <Navbar.Brand className="ms-4 brandName" href="/">
+            {props.brand}
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto">
+            <NavDropdown
+              className="me-4 ms-4 text-black"
+              title={props.items}
+              id="basic-nav-dropdown"
+              show={show}
+              onMouseEnter={showDropdown}
+              onMouseLeave={hideDropdown}
+              ref={dropdownRef}
+            >
+              <NavDropdown.Item href="#action/3.1">
+                {props.dropdownValue1}
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                {props.dropdownValue2}
+              </NavDropdown.Item> 
+              <NavDropdown.Item href="">
+                {props.dropdownValue3}
+              </NavDropdown.Item>
+            </NavDropdown>
+            <Link className="me-4 navBarLinks" to="/reservation">
+              {props.reservation}
+            </Link>
+            <Link className="me-4 navBarLinks" to="/">
+              {props.home}
+            </Link>
+            <Link className="me-4 navBarLinks" to="/contact">
+              {props.contacts}
+            </Link>
+            </Nav>
+            <Nav>
+            <Link className="ms-auto me-4 navBarLinks" to="/login">
+              {props.login}
+            </Link>
+            </Nav>
+          </Navbar.Collapse>
       </Navbar>
-    </div>
+    </>
   );
 }
 
