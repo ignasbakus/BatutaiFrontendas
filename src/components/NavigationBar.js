da94ad1396003267ad6b4cbafc04e754505e48e1
LeBrounius <ignas.bakus77@gmail.com>
2024-01-28 21:20:36 +0200

    Sukurtas offcanvas navbaras kai sumažėja ekranas

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,9 +1,9 @@
 import React from "react";
 import "../Styles/NavigationBar.css";
-import Container from 'react-bootstrap/Container'; 
-import Nav from 'react-bootstrap/Nav'; 
-import Navbar from 'react-bootstrap/Navbar'; 
-import NavDropdown from 'react-bootstrap/NavDropdown'; 
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import Offcanvas from "react-bootstrap/Offcanvas";
 import { Link } from "react-router-dom";
 import { useState, useRef, useEffect } from "react";
 
@@ -30,47 +30,52 @@ function NavigationBar(props) {
   return (
     <>
       <Navbar collapseOnSelect expand="md" className="bg-body-tertiary">
-          <Navbar.Brand className="ms-4 brandName" href="/">
-            {props.brand}
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-          <Navbar.Collapse id="responsive-navbar-nav">
+        <Navbar.Brand className="ms-4 brandName" href="/">
+          {props.brand}
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Offcanvas placement="end" id="responsive-navbar-nav">
+          <Offcanvas.Header closeButton>
+            <Offcanvas.Title>Offcanvas</Offcanvas.Title>
+          </Offcanvas.Header>
+          <Offcanvas.Body>
             <Nav className="me-auto">
-            <NavDropdown
-              className="me-4 ms-4 text-black"
-              title={props.items}
-              id="basic-nav-dropdown"
-              show={show}
-              onMouseEnter={showDropdown}
-              onMouseLeave={hideDropdown}
-              ref={dropdownRef}
-            >
-              <NavDropdown.Item href="#action/3.1">
-                {props.dropdownValue1}
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                {props.dropdownValue2}
-              </NavDropdown.Item> 
-              <NavDropdown.Item href="">
-                {props.dropdownValue3}
-              </NavDropdown.Item>
-            </NavDropdown>
-            <Link className="me-4 navBarLinks" to="/reservation">
-              {props.reservation}
-            </Link>
-            <Link className="me-4 navBarLinks" to="/">
-              {props.home}
-            </Link>
-            <Link className="me-4 navBarLinks" to="/contact">
-              {props.contacts}
-            </Link>
+              <NavDropdown
+                className="me-4 ms-4 text-black"
+                title={props.items}
+                id="basic-nav-dropdown"
+                show={show}
+                onMouseEnter={showDropdown}
+                onMouseLeave={hideDropdown}
+                ref={dropdownRef}
+              >
+                <NavDropdown.Item href="#action/3.1">
+                  {props.dropdownValue1}
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">
+                  {props.dropdownValue2}
+                </NavDropdown.Item>
+                <NavDropdown.Item href="">
+                  {props.dropdownValue3}
+                </NavDropdown.Item>
+              </NavDropdown>
+              <Link className="me-4 navBarLinks" to="/reservation">
+                {props.reservation}
+              </Link>
+              <Link className="me-4 navBarLinks" to="/">
+                {props.home}
+              </Link>
+              <Link className="me-4 navBarLinks" to="/contact">
+                {props.contacts}
+              </Link>
             </Nav>
             <Nav>
-            <Link className="ms-auto me-4 navBarLinks" to="/login">
-              {props.login}
-            </Link>
+              <Link className="me-4 navBarLinks" to="/login">
+                {props.login}
+              </Link>
             </Nav>
-          </Navbar.Collapse>
+          </Offcanvas.Body>
+        </Navbar.Offcanvas>
       </Navbar>
     </>
   );
