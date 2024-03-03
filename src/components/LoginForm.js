e95131004ea5371a4abeada9aea73d63df1b7220
LeBrounius <ignas.bakus77@gmail.com>
2024-03-03 16:25:02 +0200

    Dydziu bandymas

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -5,24 +5,33 @@ function LoginForm(props) {
   return (
     <form className="mt-5">
       <Row className="justify-content-center">
-        <Col className="border border-bottom-0" xs={12} md={3}>
+        <Col className="border border-bottom-0" xs={12} md={4}>
           <Form.Group className="mt-3" controlId="formBasicEmail">
             <Form.Label>Elektroninis paštas</Form.Label>
-            <Form.Control type="email" placeholder="Elektroninis paštas" />
+            <Form.Control
+              size="sm"
+              type="email"
+              placeholder="Elektroninis paštas"
+            />
           </Form.Group>
         </Col>
       </Row>
       <Row className="justify-content-center">
-        <Col className="border-end border-start" xs={12} md={3}>
+        <Col className="border-end border-start" xs={12} md={4}>
           <Form.Group className="mt-3" controlId="formBasicPassword">
             <Form.Label>Slaptažodis</Form.Label>
-            <Form.Control type="password" placeholder="Slaptažodis" />
+            <Form.Control size="sm" type="password" placeholder="Slaptažodis" />
           </Form.Group>
         </Col>
       </Row>
       <Row className="justify-content-center">
-        <Col className="border border-top-0" xs={12} md={3}>
-          <Button className="mt-2 mb-3" variant="primary" type="submit">
+        <Col className="border border-top-0" xs={12} md={4}>
+          <Button
+            size="sm"
+            className="mt-2 mb-3"
+            variant="primary"
+            type="submit"
+          >
             Prisijungti
           </Button>
         </Col>
--- src/components/ReservationForm.js
+++ src/components/ReservationForm.js
@@ -4,19 +4,19 @@ import { Form, Row, Col, InputGroup, Button } from "react-bootstrap";
 
 function ReservationForm(props) {
   return (
-    <Form className="mt-5">
-      <h1 className="text-center">Rezervacija</h1>
+    <Form className="mt-4">
+      <h4 className="text-center">Rezervacija</h4>
       <Row className="justify-content-center">
         <Col xs={12} md={3}>
           <Form.Group className="mt-3" controlId="formBasicName">
             <Form.Label>Vardas</Form.Label>
-            <Form.Control type="text" placeholder="Vardas" />
+            <Form.Control size="sm" type="text" placeholder="Vardas" />
           </Form.Group>
         </Col>
         <Col xs={12} md={3}>
           <Form.Group className="mt-3" controlId="formBasicLastName">
             <Form.Label>Pavardė</Form.Label>
-            <Form.Control type="text" placeholder="Pavardė" />
+            <Form.Control size="sm" type="text" placeholder="Pavardė" />
           </Form.Group>
         </Col>
       </Row>
@@ -24,13 +24,13 @@ function ReservationForm(props) {
         <Col xs={12} md={3}>
           <Form.Group className="mt-3" controlId="formBasicLastName">
             <Form.Label>Adresas</Form.Label>
-            <Form.Control type="text" placeholder="Adresas" />
+            <Form.Control size="sm" type="text" placeholder="Adresas" />
           </Form.Group>
         </Col>
         <Col xs={12} md={3}>
           <Form.Group className="mt-3">
             <Form.Label>Miestas</Form.Label>
-            <Form.Select aria-label="Default select example">
+            <Form.Select size="sm" aria-label="Default select example">
               <option value="1">Kaunas</option>
               <option value="2">Vilnius</option>
               <option value="3">Prienai</option>
@@ -42,7 +42,7 @@ function ReservationForm(props) {
         <Col xs={12} md={6}>
           <Form.Group className="mt-3" controlId="formBasicPhoneNumber">
             <Form.Label>Tel. Numeris</Form.Label>
-            <InputGroup>
+            <InputGroup size="sm">
               <InputGroup.Text id="basic-addon3">+370</InputGroup.Text>
               <Form.Control
                 id="inputNumber"
@@ -57,13 +57,17 @@ function ReservationForm(props) {
         <Col xs={12} md={6}>
           <Form.Group className="mt-3" controlId="formBasicEmail">
             <Form.Label>Elektroninis paštas</Form.Label>
-            <Form.Control type="email" placeholder="Elektroninis paštas" />
+            <Form.Control
+              size="sm"
+              type="email"
+              placeholder="Elektroninis paštas"
+            />
           </Form.Group>
         </Col>
       </Row>
       <Row className="justify-content-center">
         <Col xs={12} md={6}>
-          <Button className="mt-3" variant="primary" type="submit">
+          <Button size="sm" className="mt-3" variant="primary" type="submit">
             Rezervuoti
           </Button>
         </Col>
