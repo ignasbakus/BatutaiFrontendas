587a0d55ba6f3dbe554c8beb392de475a0d12603
LeBrounius <ignas.bakus77@gmail.com>
2024-03-03 18:33:03 +0200

    Sukurtas Item show puslapis, dar nesutvarkyta foto

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import HomePage from "./pages/HomePage";
 import Reservation from "./pages/Reservation";
 import Login from "./pages/Login";
 import Footer from "./components/Footer";
+import Items from "./pages/Items";
 
 function App() {
   return (
@@ -17,16 +18,17 @@ function App() {
         contacts="Kontaktai"
         login="Prisijungti"
         home="Pagrindinis"
-        dropdownValue1="Kažkas"
+        dropdownValue1="Batutai"
         dropdownValue2="Kažkas"
         dropdownValue3="Kažkas"
       />
-      <div className="container">
+      <div>
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/reservation" element={<Reservation />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/items" element={<Items />} />
         </Routes>
       </div>
       <Footer />
